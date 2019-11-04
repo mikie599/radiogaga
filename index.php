30061c8b8fb33bd32ada55c19aea23c21522706b
mikie599 <55782711+mikie599@users.noreply.github.com>
2019-11-04 11:37:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Radio Gaga. - Enjoy The Greatest Hits">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Radio Gaga.</title>
+</head>
+<body>
+	
+	<header id="header">
+		<div > <a id="logo" href="index.php">Radiogaga.</a> </div>
+
+		<nav id="main-navigation">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="playlist/Playlist.php">Playlist</a></li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<div id="bottom-part">
+		<h1>Welcome on Radiogaga</h1>
+		<h2 id="Header2">Enjoy the Greatest hits</h2>
+		<a id="playlist-button" href="playlist/Playlist.php">click here for the playlist</a>
+	</div>
+	
+</body>
+</html>
